4a9c9177bfa4500111ed371f68feaa6de2f02666
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2020-09-30 18:09:57 +0700

    fix building scripts

--- jb/project/tools/linux/scripts/mkimages_x64.sh
+++ jb/project/tools/linux/scripts/mkimages_x64.sh
@@ -6,7 +6,8 @@
 #   build_number     - specifies the number of JetBrainsRuntime build
 #   bundle_type      - specifies bundle to bu built; possible values:
 #                        jcef - the bundles with jcef
-#                        empty - the bundles without jcef
+#                        <empty> or nomod - the bundles without any additional modules (jcef)
+#                        fd - the fastdebug bundles with jcef
 #
 # jbrsdk-${JBSDK_VERSION}-osx-x64-b${build_number}.tar.gz
 # jbr-${JBSDK_VERSION}-osx-x64-b${build_number}.tar.gz
@@ -29,8 +30,15 @@ bundle_type=$4
 JBSDK_VERSION_WITH_DOTS=$(echo $JBSDK_VERSION | sed 's/_/\./g')
 WITH_IMPORT_MODULES="--with-import-modules=${MODULAR_SDK_PATH:=./modular-sdk}"
 JCEF_PATH=${JCEF_PATH:=./jcef_linux_x64}
+do_reset_changes=0
 
 source jb/project/tools/common.sh
+
+function do_exit() {
+  exit_code=$1
+  [ $do_reset_changes -eq 1 ] && git checkout HEAD modules.list src/java.desktop/share/classes/module-info.java
+  exit $exit_code
+}
 
 function create_jbr {
 
@@ -53,28 +61,37 @@ function create_jbr {
 
   if [ ! -z "${bundle_type}" ]; then
     cp -R ${BASE_DIR}/${JBR_BUNDLE} ${BASE_DIR}/jbr
-    cp -R ${JCEF_PATH}/* $BASE_DIR/$JBR_BUNDLE/lib || exit $?
+    cp -R ${JCEF_PATH}/* $BASE_DIR/$JBR_BUNDLE/lib || do_exit $?
   fi
   grep -v "^JAVA_VERSION" $JSDK/release | grep -v "^MODULES" >> $BASE_DIR/$JBR_BUNDLE/release
 
   echo Creating $JBR.tar.gz ...
-  tar -pcf $JBR.tar -C $BASE_DIR jbr || exit $?
+  tar -pcf $JBR.tar -C $BASE_DIR jbr || do_exit $?
   [ -f "$JBR.tar.gz" ] && rm "$JBR.tar.gz"
-  gzip $JBR.tar || exit $?
+  gzip $JBR.tar || do_exit $?
   rm -rf ${BASE_DIR}/${JBR_BUNDLE}
 }
 
-JBRSDK_BASE_NAME=jbrsdk-$JBSDK_VERSION
+JBRSDK_BASE_NAME=jbrsdk-${JBSDK_VERSION}
 WITH_DEBUG_LEVEL="--with-debug-level=release"
 RELEASE_NAME=linux-x86_64-server-release
-#git checkout -- modules.list src/java.desktop/share/classes/module-info.java
+JBSDK=${JBRSDK_BASE_NAME}-linux-x64-b${build_number}
 case "$bundle_type" in
   "jcef")
     git apply -p0 < jb/project/tools/patches/add_jcef_module.patch
+    do_reset_changes=1
     ;;
   "nomod" | "")
     bundle_type=""
     WITH_IMPORT_MODULES=""
+    ;;
+  "fd")
+    git apply -p0 < jb/project/tools/patches/add_jcef_module.patch
+    do_reset_changes=1
+    WITH_DEBUG_LEVEL="--with-debug-level=fastdebug"
+    RELEASE_NAME=linux-x86_64-server-fastdebug
+    JBRSDK_BASE_NAME=jbrsdk-${JBSDK_VERSION}-fastdebug
+    JBSDK=${JBRSDK_BASE_NAME}-linux-x64-fastdebug-b${build_number}
     ;;
 esac
 
@@ -88,12 +105,12 @@ sh configure \
   --with-version-opt=b${build_number} \
   $WITH_IMPORT_MODULES \
   --with-boot-jdk=${BOOT_JDK} \
-  --enable-cds=yes || exit $?
+  --enable-cds=yes || do_exit $?
 
-make images CONF=$RELEASE_NAME || exit $?
+make clean CONF=$RELEASE_NAME || exit $?
+make images CONF=$RELEASE_NAME || do_exit $?
 
 JSDK=build/$RELEASE_NAME/images/jdk
-JBSDK=$JBRSDK_BASE_NAME-linux-x64-b$build_number
 
 echo Fixing permissions
 chmod -R a+r $JSDK
@@ -102,31 +119,31 @@ BASE_DIR=build/$RELEASE_NAME/images
 JBRSDK_BUNDLE=jbrsdk
 
 rm -rf $BASE_DIR/$JBRSDK_BUNDLE
-cp -r $JSDK $BASE_DIR/$JBRSDK_BUNDLE || exit $?
+cp -r $JSDK $BASE_DIR/$JBRSDK_BUNDLE || do_exit $?
 
-if [ ! -z "$bundle_type" ]; then
-  cp -R ${JCEF_PATH}/* $BASE_DIR/$JBRSDK_BUNDLE/lib || exit $?
-fi
-if [ "$bundle_type" == "jcef" ]; then
+if [ "$bundle_type" == "jcef" ] || [ "$bundle_type" == "fd" ]; then
+  cp -R ${JCEF_PATH}/* $BASE_DIR/$JBRSDK_BUNDLE/lib || do_exit $?
   echo Creating $JBSDK.tar.gz ...
   sed 's/JBR/JBRSDK/g' ${BASE_DIR}/${JBRSDK_BUNDLE}/release > release
   mv release ${BASE_DIR}/${JBRSDK_BUNDLE}/release
 
   tar -pcf $JBSDK.tar --exclude=*.debuginfo --exclude=demo --exclude=sample --exclude=man \
-    -C $BASE_DIR $JBRSDK_BUNDLE || exit $?
+    -C $BASE_DIR $JBRSDK_BUNDLE || do_exit $?
   [ -f "$JBSDK.tar.gz" ] && rm "$JBSDK.tar.gz"
-  gzip $JBSDK.tar || exit $?
+  gzip $JBSDK.tar || do_exit $?
 fi
 
-create_jbr || exit $?
+create_jbr || do_exit $?
 
 if [ "$bundle_type" == "jcef" ]; then
-  make test-image || exit $?
+  make test-image CONF=$RELEASE_NAME || do_exit $?
 
   JBRSDK_TEST=$JBRSDK_BASE_NAME-linux-test-x64-b$build_number
 
   echo Creating $JBSDK_TEST.tar.gz ...
-  tar -pcf $JBRSDK_TEST.tar -C $BASE_DIR --exclude='test/jdk/demos' test || exit $?
+  tar -pcf $JBRSDK_TEST.tar -C $BASE_DIR --exclude='test/jdk/demos' test || do_exit $?
   [ -f "$JBRSDK_TEST.tar.gz" ] && rm "$JBRSDK_TEST.tar.gz"
-  gzip $JBRSDK_TEST.tar || exit $?
+  gzip $JBRSDK_TEST.tar || do_exit $?
 fi
+
+do_exit 0
